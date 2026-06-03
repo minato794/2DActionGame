2c94a26372f24be99467ca0688f9efbee296e179
240018 <240018@E0403-31>
2026-06-03 09:43:21 +0900

    なにぬ

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
